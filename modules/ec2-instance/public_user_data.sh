49f18080e4aa10597a17908a14ef69c3003387af
Yousef Mohamed <144364402+Black-5-Knight@users.noreply.github.com>
2024-09-30 03:12:45 +0300

    Update public_user_data.sh

--- modules/ec2-instance/public_user_data.sh
+++ modules/ec2-instance/public_user_data.sh
@@ -13,23 +13,5 @@ sudo systemctl start amazon-ssm-agent
 # Install Nginx
 sudo yum install -y nginx
 
-# # Create a simple HTML file
-
-# echo "<html><body> <h1> Welcome to Instance 1  </h1></body></html>" | sudo tee /usr/share/nginx/html/index.html
-# cat <<EOL | sudo tee /etc/nginx/conf.d/proxy.conf
-# server {
-#     listen 80;
-#     server_name _;  # Your EC2 instance's public IP address
-
-#     location / {
-#         proxy_pass http://public-lb-655308110.eu-north-1.elb.amazonaws.com;  # Your load balancer's DNS name
-#         proxy_set_header Host $host;
-#         proxy_set_header X-Real-IP $remote_addr;
-#         proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-#         proxy_set_header X-Forwarded-Proto $scheme;
-#     }
-# }
-# EOL
-
 # Start Nginx service
 sudo systemctl start nginx
